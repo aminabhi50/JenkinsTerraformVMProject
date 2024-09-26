3d3ca49cc4bd5196a6f555c357e2b02402158bf5
aminabhi50 <41742726+aminabhi50@users.noreply.github.com>
2024-09-26 00:11:53 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "vm_public_ip" {
   description = "Public IP of virtual machine"
-  value       = azurerm_linux_virtual_machine.prtfvm.public_ip_address
+  value       = azurerm_linux_virtual_machine.prvm.public_ip_address
 }
